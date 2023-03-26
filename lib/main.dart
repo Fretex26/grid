8793717f84c6d0afc6220e327d61f09b0a6ab382
Fretex <freddy.barragan@ci2.co>
2023-03-25 20:12:29 -0500

    Grid done

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,114 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                Expanded(
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                            color: const Color.fromARGB(255, 31, 174, 209),
+                          )
+                      ),
+                      Expanded(
+                        child: Container(
+                          color: const Color.fromARGB(255, 209, 117, 31),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                color: Colors.blueGrey,
+                                width: 5,
+                              ),
+                              image: const DecorationImage(
+                                fit: BoxFit.cover,
+                                alignment: Alignment.center,
+                                image: NetworkImage(
+                                    'https://cdn.pixabay.com/photo/2023/03/18/07/55/tawny-owl-7860086_1280.jpg'),
+                              ),
+                            )
+                          ),
+                        )
+                      ),
+                      Expanded(
+                        child: Container(
+                            color: const Color.fromARGB(255, 31, 174, 209),
+                          )
+                      )
+                    ],
+                  )
+                ),
+                Expanded(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Expanded(
+                        child: Container(
+                          color: const Color.fromARGB(255, 209, 117, 31),
+                          child: const Icon(
+                            Icons.cake,
+                            size: 100.0,
+                            color: Color.fromARGB(255, 100, 255, 100),
+                          ),
+                        )
+                      ),
+                      Expanded(
+                        child: Container(
+                          color: const Color.fromARGB(255, 31, 174, 209),
+                        )
+                      ),
+                      Expanded(
+                        child: Container(
+                          color: const Color.fromARGB(255, 209, 117, 31),
+                          child: const Icon(
+                            Icons.car_crash,
+                            size: 100.0,
+                            color: Color.fromARGB(255, 100, 255, 100),
+                          ),
+                        )
+                      )
+                    ],
+                  )
+                ),
+                Expanded(
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                            color: const Color.fromARGB(255, 31, 174, 209),
+                          )
+                      ),
+                      Expanded(
+                        child: Container(
+                          color: const Color.fromARGB(255, 209, 117, 31),
+                          child: Container(
+                            decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                fit: BoxFit.cover,
+                                alignment: Alignment.center,
+                                image: NetworkImage(
+                                    'https://cdn.pixabay.com/photo/2023/03/17/20/59/vines-7859445_1280.jpg'),
+                              ),
+                            )
+                          ),
+                        )
+                      ),
+                      Expanded(
+                        child: Container(
+                            color: const Color.fromARGB(255, 31, 174, 209),
+                          )
+                      )
+                    ],
+                  )
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
